8a5b0085c8c71c3a5f2069bb816c42565967e502
geocode-matt <mlindenx@gmail.com>
2020-06-21 14:08:31 -0500

    finalized in develop branch

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,26 @@
 // store time and date
 var now = moment().format("dddd, MMMM Do, YYYY");
 var hour = moment().format("HH");
-var timeBlockContainer = document.getElementById("timeBlockHolder")
-timeBlockContainer.innerHTML = (now);
+var time = moment().format("LT");
+var dateBlockContainer = document.getElementById("dateHolder")
+dateBlockContainer.innerHTML = (now);
+var timeBlockContainer = document.getElementById("timeHolder")
+timeBlockContainer.innerHTML = (time);
 $("now").text(moment().format('dddd MMMM Do'));
 
 // function to update colors over the time interval
 var colorUpdater = function() {
     $('.time-block').each(function(index, element) {
-        console.log(element);
         if ($(element).attr('id') === hour) {
             $(element).find("textarea").addClass("red")
+            $(element).find("textarea").prop('readonly', true);
         } else if ($(element).attr('id') < hour) {
-            $(element).find("textarea").addClass("grey") 
+            $(element).find("textarea").addClass("grey")
+            $(element).find("textarea").prop('readonly', true); 
         } else {
             $(element).find("textarea").addClass("green") 
         }
       });
-      console.log("colors updated");
 }
 
 // initially sets row colors when page opens
@@ -25,10 +28,12 @@ colorUpdater();
 
 // timer to update the date and to run the colorUpdater function
 setInterval(function() {
-    var time = moment().format('LT');
-    $(now).text(time);
+    var time = moment().format("LT");
+    $('#timeHolder').text(time);
     colorUpdater();
-}, 100000);
+    console.log(time);
+    console.log("colors updated");
+}, 10000);
 
 
 // save button pushes the input data to localStorage
@@ -51,3 +56,9 @@ $("#17 .description").val(localStorage.getItem("17"));
 $("#18 .description").val(localStorage.getItem("18"));
 $("#19 .description").val(localStorage.getItem("19"));
 $("#20 .description").val(localStorage.getItem("20"));
+
+// button to clear local storage
+$(".clearBtn").on("click", function() {
+    localStorage.clear();
+    location.reload();
+})
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+
+This is my Work Day Scheduler application!
+
+
+## Purpose of this application
+
+To help you plan your work day!
+
+
+## Accessibility
+
+This page is fully accessible.
+
+
+## Screenshot
+
+Here is a screenshot of my coding quiz application page:
+<img src="./screenshot.png">
+
+
+## Link to GitHub Repository
+
+Here is a link to the GitHub repository:
+https://github.com/geocode-matt/WorkdayScheduler
+
+
+## Link to deployed application
+
+Here is a link to the live web page!:
+https://geocode-matt.github.io/WorkdayScheduler/
