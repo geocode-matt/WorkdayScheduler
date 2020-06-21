c58fe83b9fd985cfea41eb69514d862a7cb7d508
geocode-matt <mlindenx@gmail.com>
2020-06-21 12:16:08 -0500

    color updater functionality working and save to local storage persistence working

--- Develop/script.js
+++ Develop/script.js
@@ -1,162 +1,61 @@
-var timeBlockContainer = document.getElementById("timeBlockHolder")
-
 // store time and date
 var now = moment().format("dddd, MMMM Do, YYYY");
 var hour = moment().format("HH");
-
+var timeBlockContainer = document.getElementById("timeBlockHolder")
+timeBlockContainer.innerHTML = (now);
 $("now").text(moment().format('dddd MMMM Do'));
 
-var colorUpdater = function(time) {
+// initially sets row colors when page opens
+$.each($('.time-block'), function() {
+    if ($(this).attr('id') === hour) {
+        $(this).addClass("red")
+    } else if ($(this).attr('id') < hour) {
+        $(this).addClass("grey") 
+    } else {
+        $(this).addClass("green") 
+    }
+    console.log("initial colors set");
+  });
 
+// function to update colors over the time interval
+var colorUpdater = function() {
+    $.each($('.time-block'), function() {
+        if ($(this).attr('id') === hour) {
+            $(this).addClass("red")
+        } else if ($(this).attr('id') < hour) {
+            $(this).addClass("grey") 
+        } else {
+            $(this).addClass("green") 
+        }
+      });
+      console.log("colors updated");
 }
 
+// timer to update the date and to run the colorUpdater function
 setInterval(function() {
     var time = moment().format('LT');
     $(now).text(time);
-    colorUpdater(time);
-}, 1000);
+    colorUpdater();
+}, 20000);
 
+
+// save button pushes the input data to localStorage
 $(".saveBtn").on("click", function() {
     var text = $(this).siblings(".description").val()
     var time = $(this).parent().attr("id")
     localStorage.setItem(time, text);
 })
 
-
- 
- 
-// function taskToArray(tasks) {
-//     for (var i in tasks) {
-//         if (tasks[i].
-//     }
-// }
-
-//////
-////// FUNCTIONALITY FOR ADDING/EDITING TEXT IN EACH TIME-BLOCK
-//////
-// $(".input-text").on("click", "p", function() {
-//     var text = $(this)
-//       .text()
-//       .trim()
-//       var textInput = $("<textarea>")
-//     .val(text)
-//     $(this).replaceWith(textInput)
-//     textInput.trigger("focus")
-
-//     tasks.nine = text;
-//     localStorage.setItem("tasks", tasks);
-//   });
-
-//   $(".input-text").on("blur", "textarea", function() {
-//     // get the textarea's current value/text
-//     var text = $(this)
-//       .val()
-//       .trim();
-        
-//       // recreate p element
-//     var inputP = $("<p>")
-//     .addClass("text")
-//     .text(text);
-    
-//     // replace textarea with p element
-//     $(this).replaceWith(inputP);
-//     });
-    //////
-    //////
-    //////
-
-
-
-
-
-
-
-    
-
-
-
-    // var loadTasks = function() {
-    //     tasks = JSON.parse(localStorage.getItem("tasks"));
-      
-    //     // if nothing in localStorage, create a new object to track all task status arrays
-    //     if (!tasks) {
-    //       tasks = {
-    //         nine: [],
-    //         ten: [],
-    //         eleven: [],
-    //         twelve: []
-    //       };
-    //     }
-      
-    //     // loop over object properties
-    //     $.each(tasks, function(list, arr) {
-    //       console.log(list, arr);
-    //       // then loop over sub-array
-    //       arr.forEach(function(task) {
-    //         createTask(task.text, list);
-    //       });
-    //     });
-    //   };
-      
-    //   var saveTasks = function() {
-    //     localStorage.setItem("tasks", JSON.stringify(tasks));
-    //   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var saveTasks = function() {
-//     localStorage.setItem("tasks", JSON.stringify(text));
-//     // console.log(tasks);
-// };    
-
-// $("#9am-save saveBtn").click(function() {
-//     // get form values
-//     var taskText = $("#9am-save").val();
-  
-//     if (taskText) {
-//       createTask(taskText, "9am");
-  
-//       // save in tasks array
-//       tasks.push({
-//         text: taskText,
-//       });
-  
-//       saveTasks();
-//     }
-//   })
+// Force localStorage to be persistent on page after refresh
+$("#09 .description").val(localStorage.getItem("09"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+$("#18 .description").val(localStorage.getItem("18"));
+$("#19 .description").val(localStorage.getItem("19"));
+$("#20 .description").val(localStorage.getItem("20"));
